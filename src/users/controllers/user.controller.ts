2572fa494d34c657c64718515d4b1a916293fe8e
Daniel Garcia <demonpogp@gmail.com>
2022-12-21 11:06:05 -0500

    test-gpio endpoint working

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Inject } from '@nestjs/common';
 import { UsersService } from '../domain/services/users.service';
 import { Gpio } from 'onoff';
 import { sleep } from '../../utils';
+const relay = new Gpio(17, 'out');
+
 @Controller()
 export class UserController {
   constructor(
@@ -17,11 +19,9 @@ export class UserController {
   @Get('/test-gpio')
   async testGpio() {
     const promise = new Promise<void>(async (resolve, reject) => {
-      const relay = new Gpio(17, 'out');
       relay.writeSync(1);
       await sleep(1000);
       relay.writeSync(0);
-      relay.unexport();
       resolve();
     });
     promise.then(console.log).catch(console.log);
